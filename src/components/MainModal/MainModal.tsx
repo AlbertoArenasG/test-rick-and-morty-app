1fcada4d35d4c04b0acd26942accd50e3f09f48a
Alberto Arenas <albarenasg@gmail.com>
2025-09-15 01:06:32 -0600

    feat: prepare MainModal to contains desktop and mobile sections

--- src/components/MainModal/MainModal.tsx
+++ src/components/MainModal/MainModal.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Character } from '@/lib/types';
+import Image from 'next/image';
 import styles from './MainModal.module.css';
 
 interface MainModalProps {
@@ -13,15 +14,26 @@ export default function MainModal({
   return (
     <div className={styles.modal}>
       <div className={styles.modalContent}>
-        {allCharacters.length && (
-          allCharacters.map((character) => (
-            <div
-              key={character.id}
-            >
-              <p>{character.name}</p>
+        {/* Left Section - Character Details (Desktop) */}
+        <div className={styles.leftSection}>
+        </div>
+
+        {/* Right Section - Search, Grid, Favs (Desktop) */}
+        <div className={styles.rightSection}>
+          {allCharacters.map(character => (
+            <div key={character.id}>
+              <h2>{character.name}</h2>
+              <Image
+                src={character.image}
+                alt={character.name} width={24}
+                height={24}
+              />
+              <p>Species: {character.species}</p>
+              <p>Gender: {character.gender}</p>
+              <p>Status: {character.status}</p>
             </div>
-          ))
-        )}        
+          ))}
+        </div>
       </div>
     </div>
   );
